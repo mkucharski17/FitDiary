7f8ef1483945a78792eda76db251c31648179394
Michał <m.kucharski17@wp.pl>
2019-04-13 12:53:41 +0200

    add registartion and start Activity

--- app/src/main/java/com/example/android/fitdiary/MainActivity.java
+++ app/src/main/java/com/example/android/fitdiary/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.fitdiary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,8 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the register category
+        TextView register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link RegisterActivity }
+                Intent registerIntent = new Intent(MainActivity.this, Registration.class);
+
+                //start new activity
+                startActivity(registerIntent);
+            }
+        });
+
+        // Find the View that shows the register category
+        final TextView start = findViewById(R.id.signIn);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link RegisterActivity }
+                Intent startIntent = new Intent(MainActivity.this, Start.class);
+
+                //start new activity
+                startActivity(startIntent);
+            }
+        });
+
+
     }
 
-    // Find the View that shows the numbers category
-    TextView register = findViewById(R.id.register);
+
 }
